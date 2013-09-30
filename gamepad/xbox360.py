0a1f26a2e83f247a4488b89cf790f17a767d62bd
NEricN <neric@berkeley.edu>
2013-09-30 05:00:41 -0700

    Create xbox360.py
    Been testing with gamepads for possible robot controlling.

---
+++ gamepad/xbox360.py
@@ -0,0 +1,39 @@
+#List of constants for 360 controller.
+#
+
+#Buttons
+A_BUTTON=0
+B_BUTTON=1
+X_BUTTON=2
+Y_BUTTON=3
+LEFT_BUTTON=4
+RIGHT_BUTTON=5
+BACK_BUTTON=6
+START_BUTTON=7
+L_ANALOG_BUTTON=8
+R_ANALOG_BUTTON=9
+
+#Axes
+L_ANALOG_X= 0
+L_ANALOG_Y=1 #(up is -1)
+TRIGGER=2 #(positive max)
+#R Trigger : 2 (negative max)
+R_ANALOG_Y=3 #(up is -1)
+R_ANALOG_X=4
+
+#Hat :
+HAT=0
+"""
+Pair:
+
+-1 <-X-> 1
+
+1
+^
+Y
+v
+-1
+
+X, Y
+"""
+
