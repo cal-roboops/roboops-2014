5760eb54ff8ddfdb33a6e0239b84434452e2d600
NEricN <neric@berkeley.edu>
2013-10-17 22:09:07 -0700

    simplified, deleted deprecated cold

--- robot/command.py
+++ robot/command.py
@@ -9,34 +9,16 @@ class motorManager():
 		self.motors = {}
 
 		self.motor_timeouts[FRONT_LEFT_WHEEL] = time.time()
-		self.motors[FRONT_LEFT_WHEEL_COMMAND] = FRONT_LEFT_WHEEL
-		self.motors[FRONT_LEFT_WHEEL] = FRONT_LEFT_WHEEL_COMMAND
 		self.motor_timeouts[FRONT_RIGHT_WHEEL] = time.time()
-		self.motors[FRONT_RIGHT_WHEEL_COMMAND] = FRONT_RIGHT_WHEEL
-		self.motors[FRONT_RIGHT_WHEEL] = FRONT_RIGHT_WHEEL_COMMAND
 		self.motor_timeouts[BACK_LEFT_WHEEL] = time.time()
-		self.motors[BACK_LEFT_WHEEL_COMMAND] = BACK_LEFT_WHEEL
-		self.motors[BACK_LEFT_WHEEL] = BACK_LEFT_WHEEL_COMMAND
 		self.motor_timeouts[BACK_RIGHT_WHEEL] = time.time()
-		self.motors[BACK_RIGHT_WHEEL_COMMAND] = BACK_RIGHT_WHEEL
-		self.motors[BACK_RIGHT_WHEEL] = BACK_RIGHT_WHEEL_COMMAND
 
 		self.motor_timeouts[ARM_0] = time.time()
-		self.motors[ARM_0_COMMAND] = ARM_0
-		self.motors[ARM_0] = ARM_0_COMMAND
 		self.motor_timeouts[ARM_1] = time.time()
-		self.motors[ARM_1_COMMAND] = ARM_1
-		self.motors[ARM_1] = ARM_1_COMMAND
 		self.motor_timeouts[ARM_2] = time.time()
-		self.motors[ARM_2_COMMAND] = ARM_2
-		self.motors[ARM_2] = ARM_2_COMMAND
 
 		self.motor_timeouts[CAM_X] = time.time()
-		self.motors[CAM_X_COMMAND] = CAM_X
-		self.motors[CAM_X] = CAM_X_COMMAND
 		self.motor_timeouts[CAM_Y] = time.time()
-		self.motors[CAM_Y_COMMAND] = CAM_Y
-		self.motors[CAM_Y] = CAM_Y_COMMAND
 
 		self.mode = TANK_DRIVE
 		self.queue = deque()
@@ -66,7 +48,7 @@ class motorManager():
 			self.motor_timeouts[port] = time.time() + TIMEOUT
 		else :
 			self.motor_timeouts[port] = time.time() + 300
-		self.queue_out.put(self.motors[port] + ":" + str(value) + ";")
+		self.queue_out.put(Motor(port, value).dump())
 		#send power to motors
 
 	def shut_off(self) :
@@ -74,7 +56,6 @@ class motorManager():
 
 def main():
 	r = motorManager(Queue(), Queue())
-	r.parse_commands("lel")
 
 if __name__ == "__main__" :
 	main()
