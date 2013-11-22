84a9d12a53913e32b32beeb49b8a42aed60fbd57
Eric Nguyen <ericnguyen@pioneers.berkeley.edu>
2013-11-21 20:03:24 -0800

    Added constants for easy referencing by other python programs

--- arduino/arduino.py
+++ arduino/arduino.py
@@ -4,6 +4,18 @@ from time import sleep
 portNames = ['/dev/ttyACM0', '/dev/ttyACM1', '/dev/ttyUSB0']
 arduinoIDs = ["id_arm", "id_drive", "ARM_CONTROLLER"]
 
+ARM_CW = 'q'
+ARM_STOP = 'a'
+ARM_CCW = 'z'
+
+CLAW_CLOSE = 'w'
+CLAW_STOP = 's'
+CLAW_OPEN = 'x'
+
+BASE_CW = 'e'
+BASE_STOP = 'd'
+BASE_CCW = 'c'
+
 class Arduino:
 
 	def __init__(self, uid):
