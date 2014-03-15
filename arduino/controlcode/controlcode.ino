74f5493251df32e3898a67d1a3a86094bee30e57
theclg <caseygetz@berkeley.edu>
2014-03-14 18:26:21 -0700

    Updated Controlcode

--- arduino/controlcode/controlcode.ino
+++ arduino/controlcode/controlcode.ino
@@ -11,20 +11,32 @@
 // stores the interpreted values from parsing
 int values[2];
 
+int pwmPins[4];
+int dirPins[4];
+
 const int MOTOR_ID = 0,
           SPEED = 1;
 
 // stores the read string for motor id
 char motorChar[2];
-     motorChar[1] = '\0';
+<<<<<<< HEAD
+
 
 // stores the read string for values
 char valuesChar[4];
-     valuesChar[3] = '\0';
+     
 
 // stores the read line
 char line[9];
-     line[8] = '\0';
+     
+=======
+
+// stores the read string for values
+char valuesChar[4];
+
+// stores the read line
+char line[9];
+>>>>>>> 79f7b158bd0b7f4841152b781bb3dbb0a21f92f8
 
 /* Read a whole line in serial console, wait if the serial is not available */
 void readLine(char* dist)
@@ -39,7 +51,7 @@ void readLine(char* dist)
       dist[i] = inByte;
       i++;
     }
-  } while(inByte != '\n')
+  } while(inByte != '!');
 }
 
 /* Parses a line, writes to global variables */
@@ -59,20 +71,65 @@ void parseLine(char *line)
 }
 
 
+void arm(int motor, int spd) {
+  if (spd<0) {
+    digitalWrite(pwmPins[motor], 1);
+    Serial.println(spd);
+    spd = -spd;
+  } else {
+    digitalWrite(dirPins[motor], 0);
+    Serial.println(spd);
+  }
+  analogWrite(motor+2, 255*(spd/1000));
+}
+
 void setup() {
   // initialize both serial ports:
+  motorChar[1] = '\0';
+  valuesChar[3] = '\0';
+  line[8] = '\0';
+  pwmPins[0] = 3;
+  pwmPins[1] = 5;
+  pwmPins[2] = 6;
+  pwmPins[3] = 9;
+  dirPins[0] = 2;
+  dirPins[1] = 4;
+  dirPins[2] = 7;
+  dirPins[3] = 8;
+  
   Serial.begin(9600);
+  
+<<<<<<< HEAD
+  //speed
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  //direction
+  pinMode(30, OUTPUT);
+  pinMode(31, OUTPUT);
+  pinMode(32, OUTPUT);
+  pinMode(33, OUTPUT);
+=======
+  
+  motorChar[1] = '\0';
+  valuesChar[3] = '\0';
+  line[8] = '\0';
+>>>>>>> 79f7b158bd0b7f4841152b781bb3dbb0a21f92f8
 }
+
 
 void loop() {
   // read from port 1, send to port 0:
   readLine(line);
   parseLine(line);
+  arm(values[MOTOR_ID],values[SPEED]);
   Serial.println("Parsed Info:");
   Serial.println(values[MOTOR_ID]);
   Serial.println(values[SPEED]);
   Serial.println("End of line");
 }
+
 
 
 
