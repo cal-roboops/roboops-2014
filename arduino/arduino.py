27972441da289966605dbc57067522b24fb17981
theclg <clg0000@gmail.com>
2014-03-15 03:26:15 -0700

    Updated arduino.py

--- arduino/arduino.py
+++ arduino/arduino.py
@@ -3,7 +3,7 @@ from time import sleep
 
 #todo: make smarter decision
 portNames = {};
-portNames["ARM_CONTROLLER"] = 'blah'
+portNames["ARM_CONTROLLER"] = '/dev/ttyACM1'
 portNames["DRIVE_CONTROLLER"] = 'lel'
 
 
@@ -16,16 +16,18 @@ class Arduino:
 			pass
 
 	def write(self, message):
-		#self.serial.write(message)
+		self.serial.write(message)
 		pass
 
 	def read(self):
-		#return self.serial.readline().strip()
+		return self.serial.readline().strip()
 		return "lel"
 
 if __name__ == "__main__":
-	interface = Arduino("ARM_CONTROLLER")
+	interface = Arduino(portNames["ARM_CONTROLLER"])
 	while True:
-		interface.write("HI")
+		x=raw_input("hey: ")
+		interface.write(x)
 		print("arduino says", interface.read())
+
 		sleep(1)
