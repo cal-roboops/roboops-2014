63e67839f925144685599b4ef9ff7d78076cf072
Eric Nguyen <ericnguyen@pioneers.berkeley.edu>
2014-03-19 20:35:11 -0700

    Removed references to timeouts

--- robot/command.py
+++ robot/command.py
@@ -1,4 +1,3 @@
-import time
 from collections import deque
 from constants.constants import *
 from Queue import Queue
@@ -7,29 +6,9 @@ import arduino.arduino as arduino
 
 class motorManager():
 	def __init__(self, queue_in, queue_out, arm_port, drive_port) :
-		self.motor_timeouts = {}
 		self.arduino = {}
 		self.translations = {}
 
-		self.motor_timeouts[FRONT_LEFT_WHEEL] = time.time()
-		self.motor_timeouts[FRONT_RIGHT_WHEEL] = time.time()
-		self.motor_timeouts[BACK_LEFT_WHEEL] = time.time()
-		self.motor_timeouts[BACK_RIGHT_WHEEL] = time.time()
-
-		self.motor_timeouts[FRONT_LEFT_SWERVE] = time.time()
-		self.motor_timeouts[FRONT_RIGHT_SWERVE] = time.time()
-		self.motor_timeouts[BACK_LEFT_SWERVE] = time.time()
-		self.motor_timeouts[BACK_RIGHT_SWERVE] = time.time()
-
-		self.motor_timeouts[ARM_0] = time.time()
-		self.motor_timeouts[ARM_1] = time.time()
-		self.motor_timeouts[ARM_2] = time.time()
-		self.motor_timeouts[CLAW] = time.time()
-
-		self.motor_timeouts[CAM_X] = time.time()
-		self.motor_timeouts[CAM_Y] = time.time()
-
-		self.mode = TANK_DRIVE
 		self.queue = deque()
 
 		armArduino = arduino.Arduino(arm_port)
@@ -58,20 +37,6 @@ class motorManager():
 
 		self.is_active = True
 
-	def update(self) :
-		while(self.is_active):
-			Serialize.run_robot(self.queue_in.get(), self)
-			for motor in self.motor_timeouts :
-				if self.motor_timeouts[motor] < time.time() :
-					self.update_port(motor, 0)
-			#time.sleep(0.1)
-
-	def check_timeouts(self) :
-		while(self.is_active):
-			for motor in self.motor_timeouts :
-				if self.motor_timeouts[motor] < time.time() :
-					self.update_port(motor, 0)
-
 	def emergency_stop(self) :
 		for port in self.arduino:
 			self.update_port(port, 0)
@@ -81,10 +46,6 @@ class motorManager():
 			Serialize.run_robot(self.queue_in.get(), self)
 
 	def update_port(self, port, value) :
-		if value :
-			self.motor_timeouts[port] = time.time() + TIMEOUT
-		else :
-			self.motor_timeouts[port] = time.time() + 30
 		self.queue_out.put(Serialize.Motor(port, value).dump())
 		if(self.arduino[port].active) :
 			print("Attempting to write: " + self.translate(port, value) + " to arduino.")
