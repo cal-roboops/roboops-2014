9ea7dc055ca0cc2c9a640c685455a3ca5bea7a88
Eric Nguyen <ericnguyen@pioneers.berkeley.edu>
2014-05-30 19:39:51 -0700

    fixed drive arduino for 3 roboclaws

--- arduino/driveArduino/driveArduino.ino
+++ arduino/driveArduino/driveArduino.ino
@@ -7,11 +7,25 @@
 // When using packet serial, each RoboClaw gains a specific address. To send a command to that controller, you must include the
 // controller's address to ensure that it will receive the command.
 
-// M1 is swerve, M2 is motor
+// one drives left wheels, one drives right wheels, one drives swerves
 #define ADDRESS_B_L 0x80
 #define ADDRESS_B_R 0x81
 #define ADDRESS_F_L 0x82
 #define ADDRESS_F_R 0x83
+
+#define ADDRESS_RIGHT 0x81
+#define RIGHT_M1 BACK_RIGHT_WHEEL
+#define RIGHT_M2 FRONT_RIGHT_WHEEL
+
+#define ADDRESS_LEFT 0x80
+#define LEFT_M1 BACK_LEFT_WHEEL
+#define LEFT_M2 FRONT_LEFT_WHEEL
+
+#define ADDRESS_SWERVE 0x83
+#define SWERVE_M1 FRONT_LEFT_SWERVE
+#define SWERVE_M2 FRONT_RIGHT_SWERVE
+#define SWERVE_M1_INDEX F_L_INDEX
+#define SWERVE_M2_INDEX F_R_INDEX
 
 #define ENCODER_B_L 2
 #define ENCODER_B_R 4
@@ -40,10 +54,6 @@ char addresses[4];
 
 int values[2];
 int goals[4]; //goals for servos
-int initial[4]; //initial positions for servos
-int prevDir[4]; // previous direction for servos
-int overShot[4]; // counts of how many times overshot
-int additive[4]; // hotfix to add power slightly
 
 const int MOTOR_ID = 0,
           SPEED = 1;
@@ -65,8 +75,8 @@ void setup()
   pinMode(ENCODER_F_R, INPUT);
   swerves = RoboClaw(10, 11, 100); // Pin 10 on the Arduino connects to S2 on 10 and Pin 11 connects to S1 on the RoboClaw
   
-  encoders[B_L_INDEX] = ENCODER_B_L;
-  encoders[B_R_INDEX] = ENCODER_B_R;
+  encoders[B_L_INDEX] = ENCODER_B_L; // only used to match up with indices of motors
+  encoders[B_R_INDEX] = ENCODER_B_R; // ''
   encoders[F_L_INDEX] = ENCODER_F_L;
   encoders[F_R_INDEX] = ENCODER_F_R;
   
@@ -78,17 +88,11 @@ void setup()
   for(int i = 0; i < 4; i++)
   {
     goals[i] = 20;
-    overShot[i] = 0;
-    prevDir[i] = 0;
-    additive[i] = 0;
-    //initial[i] = getPosition(encoders[i]); 
   }
-  
-  initial[2] = 0;
 
   swerves.begin(9600);
   Serial.begin(9600);
-  Serial.setTimeout(100);
+  //Serial.setTimeout(100);
 }
 
 /* Read a whole line in serial console, wait if the serial is not available */
@@ -116,14 +120,7 @@ void parseLine(char *line)
   values[SPEED] = atoi(valuesChar)* (line[3] == '1' ? -1 : 1);
 }
 
-void flushBuffer()
-{
-  while (Serial.available())
-  {
-    Serial.read();
-  }
-}
-
+// gets position of encoder at pin
 double getPosition(int pin)
 {
   double p = pulseIn(pin, HIGH);
@@ -131,7 +128,7 @@ double getPosition(int pin)
   return p;
 }
 
-
+// sets a value for a motor or encoder, depending on given ids
 void set(int motor, int value)
 {
   boolean isEncoder = 0;
@@ -139,22 +136,6 @@ void set(int motor, int value)
   
   switch(motor)
   {
-    case FRONT_LEFT_WHEEL:
-      index = F_L_INDEX;
-      isEncoder = 0;
-      break;
-    case FRONT_RIGHT_WHEEL:
-      index = F_R_INDEX;
-      isEncoder = 0;
-      break;
-    case BACK_LEFT_WHEEL:
-      index = B_L_INDEX;
-      isEncoder = 0;
-      break;
-    case BACK_RIGHT_WHEEL:
-      index = B_R_INDEX;
-      isEncoder = 0;
-      break;
     case FRONT_LEFT_SWERVE:
       index = F_L_INDEX;
       isEncoder = 1;
@@ -163,14 +144,10 @@ void set(int motor, int value)
       index = F_R_INDEX;
       isEncoder = 1;
       break;
+    // no longer have support for back swerves
     case BACK_LEFT_SWERVE:
-      index = B_L_INDEX;
-      isEncoder = 1;
-      break;
     case BACK_RIGHT_SWERVE:
-      index = B_R_INDEX;
-      isEncoder = 1;
-      break;
+      return;
   }
   
   if(isEncoder)
@@ -178,26 +155,65 @@ void set(int motor, int value)
     goals[index] = (float(value)*3.1) + 20;
     Serial.print("Set to ");
     Serial.println(goals[index]);
-    overShot[index] = 0;
   }
   else
   {
-    drive(index, value);
+    drive(motor, value);
+  }
+}
+
+// sets a robo claw to a value; value between -1000 and 1000; id is 1 or 2 (motor); address is roboclaw address
+void setRoboClaw(int id, int address, int value)
+{
+  setRoboClaw(id, address, value, 0, 1000, 0, 127);
+}
+
+// sets a robo claw to a value, fed given ranges and given output ranges
+void setRoboClaw(int id, int address, int value, int minInp, int maxInp, int minVal, int maxVal)
+{
+  if(id == 1)
+  {
+    if(value > 0)
+    {
+      swerves.ForwardM1(address, map(value, minInp, maxInp, minVal, maxVal));
+    }
+    else
+    {
+      swerves.BackwardM1(address, map(-1*value, minInp, maxInp, minVal, maxVal));
+    }
+  }
+  else
+  {
+    if(value > 0)
+    {
+      swerves.ForwardM2(address, map(value, minInp, maxInp, minVal, maxVal));
+    }
+    else
+    {
+      swerves.BackwardM2(address, map(-1*value, minInp, maxInp, minVal, maxVal));
+    }
   }
 }
 
 // drives motors
-void drive(int motorIndex, int value)
+void drive(int motor, int value)
 {
-  //Serial.println("Driving!");
-  //Serial.println(addresses[motorIndex]);
-  if(value > 0)
+  switch(motor)
   {
-    swerves.ForwardM2(addresses[motorIndex], map(value, 0, 1000, 0, 127)); 
-  }
-  else
-  {
-    swerves.BackwardM2(addresses[motorIndex], map(-1*value, 0, 1000, 0, 127)); 
+    case LEFT_M1:
+      setRoboClaw(1, ADDRESS_LEFT, value);
+      break;
+    case LEFT_M2:
+      setRoboClaw(2, ADDRESS_LEFT, value);
+      break;
+    case RIGHT_M1:
+      setRoboClaw(1, ADDRESS_RIGHT, value);
+      break;
+    case RIGHT_M2:
+      setRoboClaw(2, ADDRESS_RIGHT, value);
+      break;
+    default:
+      Serial.println("What the fuck? Driving bad motor!");    
   }
 }
 
@@ -205,85 +221,26 @@ void drive(int motorIndex, int value)
 void setAllSwerves()
 {
   float delta;
-  for (int i = 0; i<4; i++)
+  
+  delta = int(goals[SWERVE_M1_INDEX] - getPosition(encoders[SWERVE_M1_INDEX]));
+  if(delta > 10 || delta < 10)
   {
-    delta = int(goals[i] + int(initial[i] - getPosition(encoders[i]))%360);
-    //delta = delta > 180 ? -(delta - 180) : (delta < -180 ? -(delta + 180) : delta);
-    if(i==2)
-    {
-      Serial.print("Goal : ");
-      Serial.println(goals[i]);
-      Serial.print("Position : ");
-      Serial.println(getPosition(encoders[i]));
-      Serial.print("Initial : ");
-      Serial.println(initial[i]);
-      Serial.println(delta);
-    }
-
-    //Serial.println(addresses[i]);
-    if(delta > 10)
-    {
-      swerves.ForwardM1(addresses[i], map(delta, 0, 360, 40, 80));      
-    }
-    else if(delta < -10)
-    {
-      swerves.BackwardM1(addresses[i], map(delta, 0, 360, 40, 80));
-    }
-    else
-    {
-     swerves.ForwardM1(addresses[i], 0); 
-    }
-    /*
-    if(delta > 10 || delta < -10)
-    {
-      if(delta > 0)
-      {
-        if(prevDir[i] == -1)
-        {
-          overShot[i] += 0;
-          additive[i] = 0; 
-        }
-        else
-        {
-          additive[i] += 0;
-        }
-        swerves.BackwardM1(addresses[i], int(additive[i] + map(delta/(overShot[i] + 1), 0, 360, 40, 80)));//+ 50/(overShot[i] + 1)));//map(delta/(overShot[i] + 1), 0, 360, 20, 127));
-        if(i==2)
-        {
-          Serial.print("Moving forward at ");
-          Serial.println(int(additive[i] + map(delta/(overShot[i] + 1), 0, 360, 40, 80)));//int(additive[i] + 50/(overShot[i] + 1)));//map(delta/(overShot[i] + 1), 0, 360, 20, 127));
-        }
-        prevDir[i] = 1;
-      }
-      else if (delta < 0)
-      {
-        if(prevDir[i] == 1)
-        {
-          overShot[i] += 0;
-          additive[i] = 0;
-        }
-        else
-        {
-          additive[i] += 0;
-        }
-        swerves.ForwardM1(addresses[i], int(additive[i] +map(-1*delta/(overShot[i] + 1), 0, 360, 40, 80)));//+ 50/(overShot[i] + 1)));//map(-1*delta/(overShot[i] + 1), 0, 360, 20, 127));
-        if(i==2)
-         { 
-          Serial.print("Moving backward at ");
-          Serial.println(int(additive[i] +map(-1*delta/(overShot[i] + 1), 0, 360, 40, 80)));//int(additive[i] + 50/(overShot[i] + 1)));//map(-1*delta/(overShot[i] + 1), 0, 360, 20, 127));
-          
-          Serial.print(delta); Serial.println(" is less than -10.");
-         }
-        prevDir[i] = -1;
-      }
-    }
-    else
-    {
-      overShot[i] = 0;
-      additive[i] = 0;
-      swerves.ForwardM1(addresses[i], 0);
-    }
-  }*/
+    setRoboClaw(1, ADDRESS_SWERVE, delta, 0, 360, 40, 80);
+  }
+  else
+  {
+    setRoboClaw(1, ADDRESS_SWERVE, 0); 
+  }
+  
+  delta = int(goals[SWERVE_M2_INDEX] - getPosition(encoders[SWERVE_M2_INDEX]));
+  if(delta > 10 || delta < 10)
+  {
+    setRoboClaw(2, ADDRESS_SWERVE, delta, 0, 360, 40, 80);
+  }
+  else
+  {
+    setRoboClaw(2, ADDRESS_SWERVE, 0); 
+  }
 }
 
 void loop()
@@ -298,8 +255,5 @@ void loop()
   }
   Serial.println(getPosition(encoders[2]));
   setAllSwerves();
-  /*
-  Serial.println(getPosition(encoders[2]));
-  delay(10);
-  /*/
 }
+
