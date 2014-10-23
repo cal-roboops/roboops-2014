42483f058bad26a1e469034303f0bf3ef72cc636
Ngoc Buu Tran <ngoctranfire@gmail.com>
2014-10-22 19:15:37 -0700

    First slider troll

---
+++ gui-site/js/scripts.js
@@ -0,0 +1,13 @@
+// With JQuery
+$('#ex1').slider({
+	formatter: function(value) {
+		return 'Current value: ' + value;
+	}
+});
+
+// Without JQuery
+var slider = new Slider('#ex1', {
+	formatter: function(value) {
+		return 'Current value: ' + value;
+	}
+});
